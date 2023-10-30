1d4ea19c062b85b4bcdfb0e09f434a04c8dca51e
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-10-30 16:09:34 +0100

    Speichern von Informationen mit lokalem Speicher

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container=document.querySelector('.container');
 const anzahl=document.getElementById('anzahl');
 const betrag=document.getElementById('betrag');
 const ausweahlen=document.getElementById('film');
+const sitze=document.querySelectorAll('.sitz:not(.reserviert)');
 
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('sitz') && !e.target.classList.contains('reserviert')){
@@ -15,8 +16,29 @@ ausweahlen.addEventListener('change',function(){
 })
 
 function berechnen(){
-    let ausgewaehlteSitzAnzahl=container.querySelectorAll('.sitz.ausgewaehlt').length;
-    let preis=ausweahlen.value;
+    const ausgewaehlteSitze=container.querySelectorAll('.sitz.ausgewaehlt');
+
+    const ausgewaehlteSitzeArr=[];
+    const sitzeArr=[];
+    ausgewaehlteSitze.forEach(function(sitz){
+        ausgewaehlteSitzeArr.push(sitz);
+    });
+
+    sitze.forEach(function(sitz){
+        sitzeArr.push(sitz);
+    })
+
+    let ausgewaehlteSitzeIndexe=ausgewaehlteSitzeArr.map(function(sitz){
+        return sitzeArr.indexOf(sitz);
+    });
+
+    let ausgewaehlteSitzAnzahl=ausgewaehlteSitze.length;
     anzahl.innerText=ausgewaehlteSitzAnzahl;
-    betrag.innerText=ausgewaehlteSitzAnzahl*preis;
+    betrag.innerText=ausgewaehlteSitzAnzahl*ausweahlen.value;
+
+    speichernImLokalenSpeicher(ausgewaehlteSitzeIndexe);
+}
+function speichernImLokalenSpeicher(indexe){
+    localStorage.setItem('ausgewaehlteSitze',JSON.stringify(indexe));
+    localStorage.setItem('ausgewaehlteFilmIndex',ausweahlen.selectedIndex);//Ausgewähltes Film*
 }
