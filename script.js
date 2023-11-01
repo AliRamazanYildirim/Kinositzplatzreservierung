deb27904e52526af057036d6813701033d980af0
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-11-01 09:32:55 +0100

    Abrufen von Ticketinfos aus dem lokalen Speicher

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const betrag=document.getElementById('betrag');
 const ausweahlen=document.getElementById('film');
 const sitze=document.querySelectorAll('.sitz:not(.reserviert)');
 
+abrufenAusLokalSpeicher();
+berechnen();
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('sitz') && !e.target.classList.contains('reserviert')){
         e.target.classList.toggle('ausgewaehlt');
@@ -38,6 +40,24 @@ function berechnen(){
 
     speichernImLokalenSpeicher(ausgewaehlteSitzeIndexe);
 }
+function abrufenAusLokalSpeicher(){
+    const  ausgewaehlteSitze=JSON.parse(localStorage.getItem('ausgewaehlteSitze'));
+
+    if(ausgewaehlteSitze!=null && ausgewaehlteSitze.length>0){
+        sitze.forEach(function(sitz,index){
+            if(ausgewaehlteSitze.indexOf(index)>-1){
+                sitz.classList.add('ausgewaehlt');
+            }
+        });
+    }
+
+    const ausgewaehlteFilmIndex=localStorage.getItem('ausgewaehlteFilmIndex');
+
+    if(ausgewaehlteFilmIndex!=null){
+        ausweahlen.selectedIndex=ausgewaehlteFilmIndex;
+    }
+}
+
 function speichernImLokalenSpeicher(indexe){
     localStorage.setItem('ausgewaehlteSitze',JSON.stringify(indexe));
     localStorage.setItem('ausgewaehlteFilmIndex',ausweahlen.selectedIndex);//Ausgewähltes Film*
